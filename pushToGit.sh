d5d3aa513cc3b4d9c4481034db38c49a1e980942
nvdinh185 <nvdinh0905671805@gmail.com>
2019-05-08 09:11:37 +0700

    push to git

--- pushToGit.sh
+++ pushToGit.sh
@@ -1,5 +1,5 @@
-git init
+#git init
 git add .
 git commit -m "push to git"
-git remote add origin https://github.com/nvdinh185/ionic-user-form.git
+#git remote add origin https://github.com/nvdinh185/ionic-user-form.git
 git push -u origin master
